1e3f423d21494beb5d0e16e7af9d141f59e0a688
Luke <lukewu1015@gmail.com>
2024-10-28 17:44:32 +0800

    adding something to test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-arr = [[0]*10 for _ in range(9)]
-arr[0][1] = 1
+n = 10
+m = 9
+arr = [[0]*m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        arr[i][j] = i * j + 1
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end='\t')
+    print()
+
 print(arr)
